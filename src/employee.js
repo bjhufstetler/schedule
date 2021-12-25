d983b2ebe5994e7f1e85cee8dede7bcc45ac61b0
Brandon Hufstetler <bjhufstetler@gmail.com>
2021-12-24 16:25:50 -0800

    adds editPhone/Email/Address/First/Last/Role() to employee.js

--- src/employee.js
+++ src/employee.js
@@ -1,7 +1,8 @@
 class Employee {
-    constructor(first, last){
+    constructor(first, last, role){
         this.first = first,
-        this.last = last
+        this.last = last,
+        this.role = role
     }
     editPhoneNumber(arg) {
         if(Number.isInteger(arg) && arg.toString().length == 10){
@@ -15,6 +16,22 @@ class Employee {
             this.email = arg;
         } else {
             throw new Error('Not a valid email address.')
+        }
+    }
+    editAddress(arg) {
+        this.address = arg;
+    }
+    editFirst(arg) {
+        this.first = arg;
+    }
+    editLast(arg) {
+        this.last = arg;
+    }
+    editRole(arg) {
+        if(arg == "Student" || arg == "Permanent Party") {
+            this.role = arg;
+        } else {
+            throw new Error('Invalid role: must be Student or Permanent Party.')
         }
     }
 }
--- test/employee.test.js
+++ test/employee.test.js
@@ -8,7 +8,7 @@ describe('Employee', () => {
     })
     let emp;
     beforeEach(() => {
-        emp = new Employee('Brandon', 'Hufstetler');
+        emp = new Employee('Brandon', 'Hufstetler', 'Permanent Party');
     })
     test('add phone', () => {
         emp.editPhoneNumber(4798839728);
@@ -20,5 +20,24 @@ describe('Employee', () => {
         emp.editEmail('brandon.hufstetler@spaceforce.mil');
         expect(emp.email).toBe('brandon.hufstetler@spaceforce.mil');
         expect(() => emp.editEmail('@bjhufstetler')).toThrow();
+    })
+    test('add address', () => {
+        emp.editAddress('610 Cherry St, Lompoc, CA 93437')
+        expect(emp.address).toBe('610 Cherry St, Lompoc, CA 93437')
+    })
+    test('edit first', () => {
+        emp.editFirst('Drandon');
+        expect(emp.first).toBe('Drandon');
+    })
+    test('edti last', () => {
+        emp.editLast('Buffstetler');
+        expect(emp.last).toBe('Buffstetler');
+    })
+    test('edit role', () => {
+        emp.editRole('Student');
+        expect(emp.role).toBe('Student');
+        emp.editRole('Permanent Party');
+        expect(emp.role).toBe('Permanent Party');
+        expect(() => emp.editRole('Tudent')).toThrow();
     })
 })
